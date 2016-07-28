769997779645664b82c2e91d1cb42141315df4ab
Kevin Amiranoff <kamiranoff@gmail.com>
2016-07-28 02:23:13 +0200

    finishes up server side auth

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,5 +1,20 @@
+const jwt = require('jwt-simple');
+const config = require('./../config/config');
+
 const User = require('./../models/user');
 
+
+function tokenForUser(user){
+  const timestamp = new Date().getTime();
+  return jwt.encode({sub: user.id, iat: timestamp}, config.secret, null, null);
+}
+
+exports.signin = function(req,res,next) {
+  // User has already had their email and password authenticated
+  // We just need to give them a token
+
+  res.send({token: tokenForUser(req.user)});
+};
 
 exports.signup = function(req,res,next){
   console.log(req.body);
@@ -33,7 +48,7 @@ exports.signup = function(req,res,next){
     });
 
     // respond to request indication the user was created
-    res.json({success:true});
+    res.json({token:tokenForUser(user)});
 
   });
 
